fa57c52b4dc6dae4cf33a052992503219784c70a
Samson <57755235+Samson-DVS@users.noreply.github.com>
2020-02-20 13:02:01 +0800

    Create Createfolder.sh

---
+++ Semester 3/Scripting Languages/Portfolio/Portfolio1/Task1/Createfolder.sh
@@ -0,0 +1,12 @@
+#bin/bash
+#Author : Samson
+#Date : 17 Jan 2020
+
+#This command first create 
+#folder CSP2101 under Desktop
+#then inside that will
+#create Portfolio1 folder
+#Then create Task1,Task2,Task3
+#folders inside
+
+mkdir -p ~/Desktop/CSP2101/Portfolio1/{Task1,Task2,Task3}
